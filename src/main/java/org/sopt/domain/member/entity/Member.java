ce7b343e20d355af6035b1873bb62721d5ef7f5b
koobontak <kbt82883@gmail.com>
2025-10-16 14:47:01 +0900

    feat: 멤버 엔티티 필드 수정

---
+++ src/main/java/org/sopt/domain/member/entity/Member.java
@@ -0,0 +1,44 @@
+package org.sopt.domain.member.entity;
+
+import java.time.LocalDate;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private LocalDate birthday;
+    private String email;
+    private Gender gender;
+
+    public Member(Long id, String name, LocalDate birthday, String email, Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.birthday = birthday;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public static Member of(Long id, String name, LocalDate birthday, String email, Gender gender) {
+        return new Member(id, name, birthday, email, gender);
+    }
+}
